7a998743225ac6e22a1cf8990f3f7661872d6531
xiexiaoqing <7-11small@163.com>
2018-12-09 17:01:20 +0800

    linux cmd guide

---
+++ bash_cmds.sh
@@ -0,0 +1,9 @@
+###List all commands that a shell knows###
+compgen -c  # will list all commands you could run 
+compgen -a # will list all aliases you could run
+compgen -b # will list all built-ins you could run
+compgen -k # will list all keywords you could run
+compgen -A function # will list all functions you could run
+compgen -A function -abck # will list all the above in one go 
+
+
